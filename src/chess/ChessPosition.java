385e076b6c933d1549c2dc0810a18924c1ae5e77
m1ll3s <w.salomao@live.com>
2021-07-19 23:10:19 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -19,7 +19,9 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-	
+	//O código a seguir é uma implementação de referências, a coluna é um vetor de 7 posições, por isto a referência da casa
+	//digitada - 8 equivale visualmente a coluna correta que digitamos assim como usar o caracter unicode para percorrer
+	//matematicamente, ex: a-a=0, a-b=1...
 	protected Position toPosition() {
 		return new Position(8 - row, column -'a');
 	}
