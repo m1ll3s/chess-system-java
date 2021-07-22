216937ecba91452137346e7a9e1cf277fa1f77b2
m1ll3s <w.salomao@live.com>
2021-07-22 00:41:31 -0300

    Switching player each turn

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -13,21 +13,22 @@ public abstract class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	/* Aqui vamos utilizar um HookMethod onde vamos criar um método concreto
-	 * que vai retornar um método abstrato e só faz sentido quando um método 
-	 * concreto implementa um método abstrato
-	*/
+
+	/*
+	 * Aqui vamos utilizar um HookMethod onde vamos criar um método concreto que vai
+	 * retornar um método abstrato e só faz sentido quando um método concreto
+	 * implementa um método abstrato
+	 */
 	public abstract boolean[][] possibleMoves();
-	
-	public boolean possibleMove(Position position){
+
+	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
-	
+
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat.length; j++) {
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
 				if (mat[i][j]) {
 					return true;
 				}
